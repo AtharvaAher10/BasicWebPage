2d2b361539aa1559a859b64cdb9ab45d1301f03d
AtharvaAher10 <atharavaaher2019@gmail.com>
2024-05-31 18:18:01 +0530

    dynamic routing

---
+++ day22/src/component/User1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>
+      <h4>This Is User 1 Page</h4>
+    </div>
+  )
+}
+
+export default Admin
---
+++ day22/src/component/User2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Userdetails = () => {
+  return (
+    <div>
+      <h4> This is User 2 Page !</h4>
+    </div>
+  )
+}
+
+export default Userdetails;
